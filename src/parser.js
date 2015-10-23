7eee3c15477a2921d245167424193133255ba0bd
Johan Nordberg <its@johan-nordberg.com>
2015-10-23 11:14:42 +0200

    Add resolve function option to parse

--- src/parser.js
+++ src/parser.js
@@ -14,7 +14,7 @@ var SIMPLIFIED_CJS = ['require', 'exports', 'module'];
 
 
 // Convert AMD-style JavaScript string into node.js compatible module
-exports.parse = function(raw){
+exports.parse = function(raw, resolveFn){
     var output = '';
     var ast = esprima.parse(raw, {
         range: true,
@@ -39,7 +39,7 @@ exports.parse = function(raw){
         output += useStrict.expression.raw +';\n';
     }
     output += raw.substring( 0, def.range[0] ); // anything before define
-    output += getRequires(args, factory); // add requires
+    output += getRequires(args, factory, resolveFn); // add requires
     output += getBody(raw, factory.body, useStrict); // module body
 
     output += raw.substring( def.range[1], raw.length ); // anything after define
@@ -48,7 +48,7 @@ exports.parse = function(raw){
 };
 
 
-function getRequires(args, factory){
+function getRequires(args, factory, resolveFn){
     var requires = [];
     var deps = getDependenciesNames( args );
     var params = factory.params.map(function(param, i){
@@ -68,6 +68,7 @@ function getRequires(args, factory){
             // only do require for params that have a matching dependency also
             // skip "magic" dependencies
             var depName = mapLookup[param.dep] || param.dep;
+            if (resolveFn != null) { depName = resolveFn(depName); }
             requires.push( 'var '+ param.name +' = require(\''+ depName +'\');' );
         }
     });
